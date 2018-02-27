289225bd670a7a2cbfc03681b8fde46812393727
sunny sharma <sunnysanyal@outlook.com>
2018-02-28 02:14:13 +0530

    Add project files.

---
+++ ShopingWebsite(Practise)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopingWebsite_Practise_.Startup))]
+namespace ShopingWebsite_Practise_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
